7ed1420463f1b3a050851c38448c5fc5d4bd0429
Caleb Sword <caleb.m.sword@gmail.com>
2022-04-16 17:15:36 -0400

    fixed typo in server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-#sudo java -jar -D server.port=80 \
+#sudo java -jar -Dserver.port=80 \
 #  *.jar > /dev/null 2> /dev/null < /dev/null &
 #  *.jar < /dev/null &
 
